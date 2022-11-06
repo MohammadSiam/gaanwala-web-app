261b75691ebdc8059d76447378af5e73636c13c1
Rx-devs <rxdevs@gmail.com>
2022-11-06 16:36:18 +0600

    starter template

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Button from 'react-bootstrap/Button';
 
 export default function Home() {
   return (
@@ -15,7 +16,10 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
+		<h2 className="text-2xl font-bold underline">
+			Hello world!
+		  </h2>
+		<Button variant="success">Success</Button>{' '}
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
